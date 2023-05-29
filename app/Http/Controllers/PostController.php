797e6627bf5358b6bcddc355452576ece76acc4e
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-29 09:40:46 -0600

    Vista Muro y autenticacion para proteger ruta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    //Al momento de ser instanciado el constructor ejecuta lo que hay dentro de este método
+    public function __construct()
+    {
+        $this->middleware("auth");
+    }
+
     public function index(){
-        dd(auth()->user());
+        return view("dashboard");
     }
 }
