00f9e10b0e1b859864fee144cda303f625f25f58
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-28 22:24:00 -0600

    Cambios en la migracion y validacion de username

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller {
     
@@ -10,13 +13,20 @@ class RegisterController extends Controller {
         return view("auth.registrar");
     }
     public function store(Request $request){
+        //Modificar el request para agregar username. Modificar el request solo si es realmente necesario. Lo reescribimos para que el validor lo tome en cuenta antes de insertarlo a la base de datos
+        $request->request->add(["username" => Str::slug($request->username)]);
         $this->validate($request, [
             "name" => ["required", "max:30"],
-            "username" => ["required", "unique:users", "min:3", "max:20"],
+            "username" =>["required", "unique:users", "min:3", "max:20"],
             "email" => ["required", "unique:users", "email", "max:70"],
             "password" => ["required", "confirmed", "min:8"]
         ]);
 
-        dd("Creando usuario");
+        User::create([
+            "name"=> $request->get("name"),
+            "username" => $request->username,
+            "email" => $request->email,
+            "password"=> Hash::make($request->password)
+        ]);
     }
 }
