769e483fd11df2ffc588cec05f5b1b166b1c8d93
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-31 08:53:13 -0600

    Comprobar si usuario tiene sesión abierta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,19 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">DevStagram</h1>
-                <nav >
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route("login")}}">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('register')}}">Crear Cuenta</a>
-                </nav>
+                @auth
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="#">Hola <span class="font-normal"> {{ auth()->user()->username }}</span> </a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="#">Cerrar Sesión</a>
+                    </nav>
+                @endauth
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route("login")}}">Login</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('register')}}">Crear Cuenta</a>
+                    </nav>
+                @endguest
+               
             </div>
 
         </header>
