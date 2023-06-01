651640abb155718e6a7b4b2e4345b506648f35a7
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-31 23:59:33 -0600

    Roye Model Binding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,7 +13,8 @@ class PostController extends Controller
         $this->middleware("auth");
     }
 
-    public function index(){
+    public function index(User $user){
+        dd($user);
         return view("dashboard");
     }
 }
