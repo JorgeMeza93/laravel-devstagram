a5ce835b47684b4102314ac750734a169ba4b25a
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-03 22:03:56 -0600

    Añadir csrf token a formulario files

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class ImagenController extends Controller
 {
     //
-    public function stroe(){
-        return "Desde imagen controller";
+    public function store(Request $request){
+        $input = $request->all();
+        return response()->json($input);
     }
 }
