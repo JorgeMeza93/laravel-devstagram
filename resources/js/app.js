4093f3bc8324754ddbbd37da4e4be1228e49c84d
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-04 15:16:02 -0600

    Almacenar posts en la base datos

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,10 +10,10 @@ const dropzone = new Dropzone("#dropzone", {
     maxFiles: 1,
     uploadMultiple: false,
     init: function (){
-        if(document.querySelector(['name="imagen"']).value.trim() ){
+        if(document.querySelector('[name="imagen"]').value.trim() ){
             const imagenPublicada = {};
             imagenPublicada.size = 1234;
-            imagenPublicada.name = document.querySelector(['name="imagen"']).value;
+            imagenPublicada.name = document.querySelector('[name="imagen"]').value;
             this.options.addedfiles.call(this, imagenPublicada);
             this.options.thumbnail.call(this, imagenPublicada, `/uploads/${imagenPublicada.name}`);
             imagenPublicada.previewElement.classList.add("dz-success", "dz-complete");
