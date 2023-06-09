136137faa6fb7c25e59029e496ee45d2e4bd8d77
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-08 23:44:38 -0600

    recolocar urls

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,5 +3,18 @@
     {{ $post->titulo }}
 @endsection
 @section("contenido")
-    
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+            <div class="p-3">
+                <p>0 Likes</p>
+            </div>
+            <div>
+                <p class="font-bold">{{ $post->user->username }}</p>
+            </div>
+        </div>
+        <div class="md:w-1/2">
+            
+        </div>
+    </div>
 @endsection
