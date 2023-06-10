566bd3f808089f77cabb30a35552548e68839ac3
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-09 23:59:28 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::post("/posts", [PostController::class, "store"])->name("posts.store");
 Route::get("/{user:username}/posts/{post}", [PostController::class, "show"])->name("posts.show");
 
 Route::post("/imagenes", [ImagenController::class, "store"])->name("imagenes.store");
+
+
+
+Route::post("/PIPO", [ImagenController::class, "store"])->name("imagenes.store");
