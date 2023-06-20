7d603b90374ff9451cc8be61e98468b70c01e59c
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-19 23:59:10 -0600

    boton de like añadido

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,16 @@
     <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-            <div class="p-3">
+            <div class="p-3 flex items-center">
+                <form>
+                    <div class="my-4">
+                        <button type="button">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
                 <p>0 Likes</p>
             </div>
             <div>
@@ -40,7 +49,6 @@
                         <div class="mb-5">
                             <label class="mb-2 block uppercase text-gray-500 font-bold" for="comentario">Comentario Nuevo</label>
                             <textarea id="comentario" name="comentario" placeholder="Agrega un comentario" class="border p-3 w-full rounded-lg @error("comentario") border-red-500 @enderror">
-                                
                             </textarea>
                             @error('comentario')
                                 <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
