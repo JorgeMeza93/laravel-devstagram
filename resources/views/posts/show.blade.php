1df906430f4dc599ef8a61d734cb31310f422c2e
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-21 09:19:23 -0600

    contar likes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -33,7 +33,8 @@
                         </form>
                     @endif
                 @endauth
-                <p>0 Likes</p>
+                <p class="font-bold">{{ $post->likes->count() }}<span class="font-normal"> Likes</span></p>
+
             </div>
             <div>
                 <p class="font-bold">{{ $post->user->username }}</p>
