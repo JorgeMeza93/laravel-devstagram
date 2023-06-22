bd86bae44ef46376b8821b1740530690cd9ae0a6
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-21 23:52:02 -0600

    añadido controlador de perfil del usuario

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
-use App\Http\Controllers\ComentarioController;
-use App\Http\Controllers\ImagenController;
-use App\Http\Controllers\LikeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ComentarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,6 +40,10 @@ Route::post("/{user:username}/posts/{post}", [ComentarioController::class, "stor
 Route::delete("/posts/{post}", [PostController::class, "destroy"])->name("posts.destroy");
 Route::post("/posts/{post}/likes", [LikeController::class, "store"])->name("posts.likes.store");
 Route::delete("/posts/{post}/likes", [LikeController::class, "destroy"])->name("posts.likes.destroy");
+
+// Rutas para el perfil
+Route::get("{user:username}/editar-perfil", [PerfilController::class, "index"])->name("perfil.index");
+Route::post("{user:username}/editar-perfil", [PerfilController::class, "store"])->name("perfil.store");
 
 Route::post("/imagenes", [ImagenController::class, "store"])->name("imagenes.store");
 
