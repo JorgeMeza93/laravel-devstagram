8f63af32788d1217d030a6b08320bc736433dec7
Jorge Meza Bello <jorberiver@gmail.com>
2023-06-27 23:55:28 -0600

    controlador followers

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,5 +54,10 @@ class User extends Authenticatable
         // Un susario puede tener múltiples likes
         return $this->hasMany(Like::class);
     }
+    // Almacena los seguidores de un usuario
+    public function followers(){
+        return $this->belongsToMany(User::class, "followers", "user_id", "follower_id"); 
+    }
 
+    // Almacenar los que seguimos
 }
