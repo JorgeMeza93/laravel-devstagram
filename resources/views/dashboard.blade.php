3e5d4b63ec4ab7f52421bb8fbf1d0ad8413bb770
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-02 23:56:50 -0600

    Añadir numero de seguidores

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,7 @@
                     @endauth
                 </div>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
-                    0 <span class="font-normal">Seguidores</span>
+                    {{ $user->followers->count() }} <span class="font-normal">Seguidores</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
                     0 <span class="font-normal">Siguiendo</span>
