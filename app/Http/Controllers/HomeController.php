7c03c9c364fb64cf4b6bf5a9fd790c6ec9144eae
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-03 01:37:06 -0600

    Obtener usuarios que seguimos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
+        // Obtener a quienes seguimos
+        dd( auth()->user()->followings->pluck("username", "id" )->toArray());
         return view("home");
     }
 }
